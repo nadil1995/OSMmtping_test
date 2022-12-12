be4944293f17d3a705133a9876d743d1692de817
Nadil Fernando <nadileshan1995@gmail.com>
2022-12-12 21:47:53 +0530

    testing Calculating Travel Times

--- path.py
+++ path.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt #installs with osmnx
 
 # download/model a street network for some city then visualize it
 G = ox.graph_from_place("Adelaide, Adelaide City Council, South Australia, Australia ", 
-network_type="drive")
+network_type="drive_service")
 fig, ax = ox.plot_graph(G,figsize=(20,20),bgcolor='#FFFFFF',
     node_color='black', node_size=0)
